1f752905c6a7798139105a130d1d36c1c8d66417
Ajila K Johnkutty <kjajila@gmail.com>
2023-12-14 20:00:10 -0500

    handle writing into excel

--- src/test/java/scrapper/Hypothyroidism.java
+++ src/test/java/scrapper/Hypothyroidism.java
@@ -1,5 +1,6 @@
 package scrapper;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,10 +10,14 @@ import org.openqa.selenium.WindowType;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import utilities.XLManager;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
 
 public class Hypothyroidism extends BaseClass{
+	static XLManager XLManagerOBJ=new XLManager(".//recipe.xlsx");
 	static WebDriverWait wait;
 	static JavascriptExecutor js;
 	@Test
@@ -35,51 +40,70 @@ public class Hypothyroidism extends BaseClass{
 		String urltoht = driver.getCurrentUrl();
 		Assert.assertEquals(urltoht, "https://www.tarladalal.com/recipes-for-hypothyroidism-veg-diet-indian-recipes-849" );
 		System.out.println("URL matching --> Part executed" + urltoht);
-
-		List<WebElement> recipes = driver.findElements(By.xpath("//article[@class='rcc_recipecard']"));
-
-		//public void getallRecipe() {
-		// To get the list of Recipes from single page (page no.1)
-//		       
-//		        int RowCount = recipes.size();
+		try {
+			XLManagerOBJ.setCellData("Sheet1",0,0,"Recipe ID");
+			XLManagerOBJ.setCellData("Sheet1",1,0,"Recipe Name");
+			XLManagerOBJ.setCellData("Sheet1",2,0,"Recipe Category(Breakfast/lunch/snack/dinner)");
+			XLManagerOBJ.setCellData("Sheet1",3,0,"Food Category(Veg/non-veg/vegan/Jain)");
+			XLManagerOBJ.setCellData("Sheet1",4,0,"Ingredients");
+			XLManagerOBJ.setCellData("Sheet1",5,0,"Preparation Time");
+			XLManagerOBJ.setCellData("Sheet1",6,0,"Cooking Time");
+			XLManagerOBJ.setCellData("Sheet1",7,0,"Preparation method");
+			XLManagerOBJ.setCellData("Sheet1",8,0,"Nutrient values");
+			XLManagerOBJ.setCellData("Sheet1",9,0,"Targetted morbid conditions (Diabeties/Hypertension/Hypothyroidism)");
+			XLManagerOBJ.setCellData("Sheet1",10,0,"Recipe URL");
+//			XLManagerOBJ.setCellData("Sheet1",11,0,"Testing");
+//			XLManagerOBJ.setCellData("Sheet1",12,0,"Testing");
+//			XLManagerOBJ.setCellData("Sheet1",13,0,"Testing");
+//			XLManagerOBJ.setCellData("Sheet1",14,0,"Testing");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		List<WebElement> recipes = driver.findElements(By.xpath("//article[@class='rcc_recipecard']"));
+//
+//		//public void getallRecipe() {
+//		// To get the list of Recipes from single page (page no.1)
 ////		       
-		        String ParentWindow = driver.getWindowHandle();
-		       
-		        for(WebElement recipe : recipes) {
-		        System.out.println(recipe.getAttribute("id"));
-		        WebElement recipieEle = recipe.findElement(By.xpath("//*[@id=\""+recipe.getAttribute("id")+"\"]//*[@class=\"rcc_rcpno\"]"));
-		             System.out.println("Recipie Id: "+recipieEle.getText().lines().findFirst().get());
-		             WebElement recipieNamEle = recipe.findElement(By.xpath("//*[@id=\""+recipe.getAttribute("id")+"\"]//*[@class=\"rcc_recipename\"]//*[@itemprop='url']"));
-		             System.out.println("Recipie Name: "+recipieNamEle.getText());
-		             String recipieUrl = recipieNamEle.getAttribute("href");
-		             System.out.println("Recipie Url: "+recipieUrl);
-		             
-		            // recipieNamEle.click();
-		             driver.switchTo().newWindow(WindowType.TAB);
-		             driver.navigate().to(recipieUrl);
-		             
-		             WebElement recipiePage = driver.findElement(By.xpath("//*[@id=\"maincontent\"]"));
-		             WebElement prepTimeEle = recipiePage.findElement(By.xpath("//*[@itemprop=\"prepTime\"]"));
-		             System.out.println("Preparation Time: "+prepTimeEle.getText());
-		             WebElement cookTimeEle = recipiePage.findElement(By.xpath("//*[@itemprop=\"cookTime\"]"));
-		             System.out.println("Cooking Time: "+cookTimeEle.getText());
-		             
-		             List<WebElement> ingredients = recipiePage.findElements(By.xpath("//*[@id=\"rcpinglist\"]//*[@itemprop=\"recipeIngredient\"]"));
-		             
-		             System.out.println("--Ingredients---");
-		             for(WebElement ingdnt : ingredients) {
-		            	 System.out.println(ingdnt.getText());
-		             }
-		             System.out.println("----------------");
-		             
-		             WebElement nutrientsData = recipiePage.findElement(By.xpath("//*[@id=\"rcpnutrients\"]"));
-		             System.out.println("--Nutritional Values---");
-		             System.out.println(nutrientsData.getText());
-		             System.out.println("----------------");
-		             
-		            driver.close();            
-		            driver.switchTo().window(ParentWindow);
-		        }
+////		        int RowCount = recipes.size();
+//////		       
+//		        String ParentWindow = driver.getWindowHandle();
+//		       
+//		        for(WebElement recipe : recipes) {
+//		        System.out.println(recipe.getAttribute("id"));
+//		        WebElement recipieEle = recipe.findElement(By.xpath("//*[@id=\""+recipe.getAttribute("id")+"\"]//*[@class=\"rcc_rcpno\"]"));
+//		             System.out.println("Recipie Id: "+recipieEle.getText().lines().findFirst().get());
+//		             WebElement recipieNamEle = recipe.findElement(By.xpath("//*[@id=\""+recipe.getAttribute("id")+"\"]//*[@class=\"rcc_recipename\"]//*[@itemprop='url']"));
+//		             System.out.println("Recipie Name: "+recipieNamEle.getText());
+//		             String recipieUrl = recipieNamEle.getAttribute("href");
+//		             System.out.println("Recipie Url: "+recipieUrl);
+//		             
+//		            // recipieNamEle.click();
+//		             driver.switchTo().newWindow(WindowType.TAB);
+//		             driver.navigate().to(recipieUrl);
+//		             
+//		             WebElement recipiePage = driver.findElement(By.xpath("//*[@id=\"maincontent\"]"));
+//		             WebElement prepTimeEle = recipiePage.findElement(By.xpath("//*[@itemprop=\"prepTime\"]"));
+//		             System.out.println("Preparation Time: "+prepTimeEle.getText());
+//		             WebElement cookTimeEle = recipiePage.findElement(By.xpath("//*[@itemprop=\"cookTime\"]"));
+//		             System.out.println("Cooking Time: "+cookTimeEle.getText());
+//		             
+//		             List<WebElement> ingredients = recipiePage.findElements(By.xpath("//*[@id=\"rcpinglist\"]//*[@itemprop=\"recipeIngredient\"]"));
+//		             
+//		             System.out.println("--Ingredients---");
+//		             for(WebElement ingdnt : ingredients) {
+//		            	 System.out.println(ingdnt.getText());
+//		             }
+//		             System.out.println("----------------");
+//		             
+//		             WebElement nutrientsData = recipiePage.findElement(By.xpath("//*[@id=\"rcpnutrients\"]"));
+//		             System.out.println("--Nutritional Values---");
+//		             System.out.println(nutrientsData.getText());
+//		             System.out.println("----------------");
+//		             
+//		            driver.close();            
+//		            driver.switchTo().window(ParentWindow);
+//		        }
 		   
 		     }
 
