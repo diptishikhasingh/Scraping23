ddeed655047986c49e293b2f8c4a9424ccdfa02c
Ajila K Johnkutty <kjajila@gmail.com>
2023-12-18 18:30:42 -0500

    refactor the code

--- src/test/java/scrapper/Hypothyroidism.java
+++ src/test/java/scrapper/Hypothyroidism.java
@@ -35,6 +35,15 @@ public class Hypothyroidism extends BaseClass{
 	public static String toAddIngredients="Saltwater fish|Tuna|Salmon|Wahoo|Red Snapper|Drum|Black drum|redfish|oyester|shellfish|Egg|Dairy|milk|Yogurt|Butter|Cheese|cream|Nut|Chicken|Pumpkin seed|Seaweed|Iodized salt|Brazil nut|Blue berry|Blue berries|Low-fat yogurt|Brown rice|quinoa|Mushroom";
 	public static String toAddRecipeName="Saltwater fish|Tuna|Salmon|Wahoo|Red Snapper|Drum|Black drum|redfish|oyester|shellfish|Egg|Dairy|milk|Yogurt|Butter|Cheese|cream|Nut|Chicken|Pumpkin seed|Seaweed|Iodized salt|Brazil nut|Blue berry|Blue berries|Low-fat yogurt|Brown rice|quinoa|Mushroom";
 	
+	public static String recipieId="";
+	public static String recipieName="";
+	public static String recipieUrl="";
+	public static String preparationTime="";
+	public static String cookingTime="";
+	public static String ingredientsForExcel="";
+	public static String preparation="";
+	public static String nutrients="";
+	
 	public static void init() {
 		
 		driver.findElement(By.xpath("//div[contains(text(),'RECIPES')]")).click();
@@ -62,13 +71,33 @@ public class Hypothyroidism extends BaseClass{
 			excelOBJ.setCellData(sheetName,0,6,"Cooking Time");
 			excelOBJ.setCellData(sheetName,0,7,"Preparation method");
 			excelOBJ.setCellData(sheetName,0,8,"Nutrient values");
-			excelOBJ.setCellData(sheetName,0,9,"Targetted morbid conditions (Diabeties/Hypertension/Hypothyroidism)");
+			excelOBJ.setCellData(sheetName,0,9,"Targetted morbid conditions");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	
+	public static void excelInsertData(XLManager excelOBJ, String sheetName,int row) {
+		 try {
+			 excelOBJ.setCellData(sheetName,row,0,recipieId);
+	    	 excelOBJ.setCellData(sheetName,row,1,recipieName);
+	    	 excelOBJ.setCellData(sheetName,row,2,"Recipe Category(Breakfast/lunch/snack/dinner)");
+	    	 excelOBJ.setCellData(sheetName,row,3,"Food Category(Veg/non-veg/vegan/Jain)");
+	    	 excelOBJ.setCellData(sheetName,row,4,ingredientsForExcel);
+	    	 excelOBJ.setCellData(sheetName,row,5,preparationTime);
+	    	 excelOBJ.setCellData(sheetName,row,6,cookingTime);
+	    	 excelOBJ.setCellData(sheetName,row,7,preparation);
+	    	 excelOBJ.setCellData(sheetName,row,8,nutrients);
+	    	 excelOBJ.setCellData(sheetName,row,9,"Hypothyroidism)");
+	    	 excelOBJ.setCellData(sheetName,row,10,recipieUrl);
+		 } catch (IOException e) {
+				e.printStackTrace();
+		}
+	}
+	
 	public static void extractData(String fromIngredients,String fromName,XLManager excelOBJ, String sheetName) throws IOException {
+	
 	String ParentWindow = driver.getWindowHandle();
 	List <WebElement> pages=new ArrayList<WebElement>();
 	pages=driver.findElements(By.xpath("//div[@id='pagination']/a"));
@@ -92,84 +121,84 @@ public class Hypothyroidism extends BaseClass{
 			
 //			System.out.println(htRecipeCard.getAttribute("id"));
 	        WebElement recipieEle = htRecipeCard.findElement(By.xpath("//*[@id=\""+htRecipeCard.getAttribute("id")+"\"]//*[@class=\"rcc_rcpno\"]"));
-	        String recipieId= recipieEle.getText().lines().findFirst().get();
+	        recipieId= recipieEle.getText().lines().findFirst().get();
 	        WebElement recipieNameEle = htRecipeCard.findElement(By.xpath("//*[@id=\""+htRecipeCard.getAttribute("id")+"\"]//*[@class=\"rcc_recipename\"]//*[@itemprop='url']"));
-	        String recipieName= recipieNameEle.getText();
-	        String recipieUrl = recipieNameEle.getAttribute("href");
+	        recipieName= recipieNameEle.getText();
+	        recipieUrl = recipieNameEle.getAttribute("href");
 	        
-        	 Pattern searchToEliminateName=Pattern.compile(fromName,Pattern.CASE_INSENSITIVE);
-        	 Matcher matchName=searchToEliminateName.matcher(recipieName);
-        	 if(matchName.find()) {
-        		System.out.println("****************** TO ELIMINATE **************************************");
- 			    System.out.println("Recipie Name: "+recipieNameEle.getText());
- 			    System.out.println("Recipie Url: "+recipieUrl);
-        		matchFound=true;
-        		continue;
-        	 }
+	       	 Pattern searchByName=Pattern.compile(fromName,Pattern.CASE_INSENSITIVE);
+	       	 Matcher matchName=searchByName.matcher(recipieName);
+	       	 if(matchName.find()) {
+			    System.out.println("Recipie Name: "+recipieNameEle.getText());
+			    System.out.println("Recipie Url: "+recipieUrl);
+	       		matchFound=true;
+	       		if (!(sheetName.equalsIgnoreCase("HypothyroidismAdd"))){
+	  				 continue;
+	  			 }
+	       	 }
 	        
-             driver.switchTo().newWindow(WindowType.TAB);
-             driver.navigate().to(recipieUrl);
-             WebElement singleRecipiePage = driver.findElement(By.id("maincontent"));
-             WebElement prepTimeEle = singleRecipiePage.findElement(By.xpath("//*[@itemprop=\"prepTime\"]"));
-             String preparationTime= prepTimeEle.getText();
-             WebElement cookTimeEle = singleRecipiePage.findElement(By.xpath("//*[@itemprop=\"cookTime\"]"));
-             String cookingTime =cookTimeEle.getText();
+            driver.switchTo().newWindow(WindowType.TAB);
+            driver.navigate().to(recipieUrl);
+            WebElement singleRecipiePage = driver.findElement(By.id("maincontent"));
+            WebElement prepTimeEle = singleRecipiePage.findElement(By.xpath("//*[@itemprop=\"prepTime\"]"));
+            preparationTime= prepTimeEle.getText();
+            WebElement cookTimeEle = singleRecipiePage.findElement(By.xpath("//*[@itemprop=\"cookTime\"]"));
+            cookingTime =cookTimeEle.getText();
 	             
-             List<WebElement> ingredients = new ArrayList<WebElement>();
-             ingredients = singleRecipiePage.findElements(By.xpath("//*[@id=\"rcpinglist\"]//*[@itemprop=\"recipeIngredient\"]"));
-             
-             String ingredientsForExcel="";
-             for(WebElement ingdnt : ingredients) {
-            	 Pattern searchToEliminateIngredients=Pattern.compile(fromIngredients,Pattern.CASE_INSENSITIVE);
-            	 String ing=ingdnt.getText();
-            	 Matcher matchIngredients=searchToEliminateIngredients.matcher(ing);
-            	 if(matchIngredients.find()) {
-            		 System.out.println("****************** TO ELIMINATE BY CHECKING INGREDIENTS **************************************");
-            		 matchFound=true;
-            		 continue;
-            	 }
-            	 ingredientsForExcel+=ingdnt.getText()+"    ";
-             }
-             
-             List<WebElement> preparationMethods = new ArrayList<WebElement>();
-             preparationMethods = singleRecipiePage.findElements(By.id("recipe_small_steps"));
-             String preparation="";
-             for(WebElement preparationMethod : preparationMethods) {
-            	 preparation+=", "+preparationMethod.getText();
-//            	 preparation+=preparationMethod.getText()+"\r\n";
-             }
-             
-             String nutrients="";
-             try {
+            List<WebElement> ingredients = new ArrayList<WebElement>();
+            ingredients = singleRecipiePage.findElements(By.xpath("//*[@id=\"rcpinglist\"]//*[@itemprop=\"recipeIngredient\"]"));
+            
+            ingredientsForExcel="";
+            for(WebElement ingdnt : ingredients) {
+	           	 Pattern searchToEliminateIngredients=Pattern.compile(fromIngredients,Pattern.CASE_INSENSITIVE);
+	           	 String ing=ingdnt.getText();
+	           	 Matcher matchIngredients=searchToEliminateIngredients.matcher(ing);
+	           	 if(matchIngredients.find()) {
+	           		 System.out.println("****************** TO SEARCH BY CHECKING INGREDIENTS **************************************");
+	           		 matchFound=true;
+	           		 if (!(sheetName.equalsIgnoreCase("HypothyroidismAdd"))){
+	           				 continue;
+	           			 }
+	           		 }
+	           		
+	           	 ingredientsForExcel+=ingdnt.getText()+"    ";
+            }
+            
+            List<WebElement> preparationMethods = new ArrayList<WebElement>();
+            preparationMethods = singleRecipiePage.findElements(By.id("recipe_small_steps"));
+            preparation="";
+            for(WebElement preparationMethod : preparationMethods) {
+           	 preparation+=", "+preparationMethod.getText();
+//           	 preparation+=preparationMethod.getText()+"\r\n";
+            }
+            
+            nutrients="";
+            try {
 	             WebElement nutrientsData = singleRecipiePage.findElement(By.id("rcpnuts"));
 	             nutrients=nutrientsData.getText();
-            
-             } catch (Exception e) {
+           
+            } catch (Exception e) {
 	    			e.printStackTrace();
 	    	}
-             
-             driver.close();            
-             driver.switchTo().window(ParentWindow);
-             
-             if(!matchFound) {
-            	 excelOBJ.setCellData(sheetName,row,0,recipieId);
-            	 excelOBJ.setCellData(sheetName,row,1,recipieName);
-            	 excelOBJ.setCellData(sheetName,row,2,"Recipe Category(Breakfast/lunch/snack/dinner)");
-            	 excelOBJ.setCellData(sheetName,row,3,"Food Category(Veg/non-veg/vegan/Jain)");
-            	 excelOBJ.setCellData(sheetName,row,4,ingredientsForExcel);
-            	 excelOBJ.setCellData(sheetName,row,5,preparationTime);
-            	 excelOBJ.setCellData(sheetName,row,6,cookingTime);
-            	 excelOBJ.setCellData(sheetName,row,7,preparation);
-            	 excelOBJ.setCellData(sheetName,row,8,nutrients);
-            	 excelOBJ.setCellData(sheetName,row,9,"Hypothyroidism)");
-            	 excelOBJ.setCellData(sheetName,row,10,recipieUrl);
-    			
-		        row++;
-             }
-	       
+            
+            driver.close();            
+            driver.switchTo().window(ParentWindow);
+            if (sheetName.equalsIgnoreCase("HypothyroidismEliminate")||sheetName.equalsIgnoreCase("HypothyroidismAllergies")) {
+	           	 if(!matchFound) {
+	               	 excelInsertData(excelOBJ,sheetName,row);
+	               	 row++;
+	                }
+            }
+            else {
+           	 if(matchFound) {
+               	 excelInsertData(excelOBJ, "HypothyroidismAdd",row);
+               	 row++;
+                }
+            } 
 		 }
 	}
 }
+
 	
 	@Test
 	public static void filterRecipes() throws IOException{
@@ -198,110 +227,11 @@ public class Hypothyroidism extends BaseClass{
 		
 		excelColumnHeading(XLManagerOBJ,"HypothyroidismAdd");
 		
-		String ParentWindow = driver.getWindowHandle();
-		List <WebElement> pages=new ArrayList<WebElement>();
-		pages=driver.findElements(By.xpath("//div[@id='pagination']/a"));
-		int len=pages.size();
-		String page;
-		int row=1;
-		for(int i=0;i<len;i++) {
-			page="(//div[@id='pagination']/a)[text()='"+(i+1)+"']";
-
-			driver.findElement(By.xpath(page)).click();
-
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
-			List <WebElement> htRecipeCards;
-
-			htRecipeCards=driver.findElements(By.xpath("//article[@class='rcc_recipecard']"));
-
-			for(WebElement htRecipeCard : htRecipeCards ) {
-				
-				matchFound=false;
-				
-//				System.out.println(htRecipeCard.getAttribute("id"));
-		        WebElement recipieEle = htRecipeCard.findElement(By.xpath("//*[@id=\""+htRecipeCard.getAttribute("id")+"\"]//*[@class=\"rcc_rcpno\"]"));
-		        String recipieId= recipieEle.getText().lines().findFirst().get();
-		        WebElement recipieNameEle = htRecipeCard.findElement(By.xpath("//*[@id=\""+htRecipeCard.getAttribute("id")+"\"]//*[@class=\"rcc_recipename\"]//*[@itemprop='url']"));
-		        String recipieName= recipieNameEle.getText();
-		        String recipieUrl = recipieNameEle.getAttribute("href");
-		        
-	        	 Pattern searchToEliminateName=Pattern.compile(toAddRecipeName,Pattern.CASE_INSENSITIVE);
-	        	 Matcher matchName=searchToEliminateName.matcher(recipieName);
-	        	 if(matchName.find()) {
-	        		System.out.println("****************** TO ADD **************************************");
-	 			    System.out.println("Recipie Name: "+recipieNameEle.getText());
-	 			    System.out.println("Recipie Url: "+recipieUrl);
-	        		matchFound=true;
-//	        		break;
-	        	 }
-		        
-	             driver.switchTo().newWindow(WindowType.TAB);
-	             driver.navigate().to(recipieUrl);
-	             WebElement singleRecipiePage = driver.findElement(By.id("maincontent"));
-	             WebElement prepTimeEle = singleRecipiePage.findElement(By.xpath("//*[@itemprop=\"prepTime\"]"));
-	             String preparationTime= prepTimeEle.getText();
-	             WebElement cookTimeEle = singleRecipiePage.findElement(By.xpath("//*[@itemprop=\"cookTime\"]"));
-	             String cookingTime =cookTimeEle.getText();
-		             
-	             List<WebElement> ingredients = new ArrayList<WebElement>();
-	             ingredients = singleRecipiePage.findElements(By.xpath("//*[@id=\"rcpinglist\"]//*[@itemprop=\"recipeIngredient\"]"));
-	             
-	             String ingredientsForExcel="";
-	             for(WebElement ingdnt : ingredients) {
-	            	 Pattern searchToEliminateIngredients=Pattern.compile(toAddIngredients,Pattern.CASE_INSENSITIVE);
-	            	 String ing=ingdnt.getText();
-	            	 Matcher matchIngredients=searchToEliminateIngredients.matcher(ing);
-	            	 if(matchIngredients.find()) {
-	            		 System.out.println("****************** TO ADD BY CHECKING INGREDIENTS **************************************");
-	            		 matchFound=true;
-	            		 break;
-	            	 }
-	            	 ingredientsForExcel+=ingdnt.getText()+"    ";
-	             }
-	             
-	             
-	             List<WebElement> preparationMethods = new ArrayList<WebElement>();
-	             preparationMethods = singleRecipiePage.findElements(By.id("recipe_small_steps"));
-	             String preparation="";
-	             for(WebElement preparationMethod : preparationMethods) {
-	            	 preparation+=", "+preparationMethod.getText();
-//	            	 preparation+=preparationMethod.getText()+"\r\n";
-	             }
-	             
-	             String nutrients="";
-	             try {
-		             WebElement nutrientsData = singleRecipiePage.findElement(By.id("rcpnuts"));
-		             nutrients=nutrientsData.getText();
-	            
-	             } catch (Exception e) {
-		    			e.printStackTrace();
-		    	}
-	             
-	             driver.close();            
-	             driver.switchTo().window(ParentWindow);
-	             
-	             if(matchFound) {
-	            	 XLManagerOBJ.setCellData("HypothyroidismAdd",row,0,recipieId);
-	            	 XLManagerOBJ.setCellData("HypothyroidismAdd",row,1,recipieName);
-	            	 XLManagerOBJ.setCellData("HypothyroidismAdd",row,2,"Recipe Category(Breakfast/lunch/snack/dinner)");
-	            	 XLManagerOBJ.setCellData("HypothyroidismAdd",row,3,"Food Category(Veg/non-veg/vegan/Jain)");
-	            	 XLManagerOBJ.setCellData("HypothyroidismAdd",row,4,ingredientsForExcel);
-	            	 XLManagerOBJ.setCellData("HypothyroidismAdd",row,5,preparationTime);
-	            	 XLManagerOBJ.setCellData("HypothyroidismAdd",row,6,cookingTime);
-	            	 XLManagerOBJ.setCellData("HypothyroidismAdd",row,7,preparation);
-	            	 XLManagerOBJ.setCellData("HypothyroidismAdd",row,8,nutrients);
-	            	 XLManagerOBJ.setCellData("HypothyroidismAdd",row,9,"Hypothyroidism)");
-	            	 XLManagerOBJ.setCellData("HypothyroidismAdd",row,10,recipieUrl);
-	    			
-			        row++;
-	             }
-		       
-			 }
-		}
+		extractData(toAddIngredients,toAddRecipeName,XLManagerOBJ,"HypothyroidismAdd");
 	}
 }
 		
+
 		
 
 
